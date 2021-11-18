bc56dd83e8c29f300451e5a10b802b4ced0cb8b5
doomed51 <doomed51@gmail.com>
2021-11-18 00:39:02 -0500

    cleaned up code structure and commenting. Added graphs/plots and stats. Emabled alert analysis of single symbol

--- UW_Alerts.py
+++ UW_Alerts.py
@@ -10,6 +10,7 @@ import seaborn as sns
 
 
 filePath = r"F:\workbench\UW_Alerts\UW_alerts.xlsx"
+sheetName =  'SABR'  #'ask1IV50to200' 
 #filePath = r"F:\workbench\Sandbox\splittest.xlsx"
 
 print('')
@@ -21,7 +22,7 @@ print('')
 
 #load workbook
 workbook = load_workbook(filename=filePath)
-rawDF = pd.read_excel(filePath, 'ask1IV50to200')
+rawDF = pd.read_excel(filePath, sheetName)
 
 #####
 # Util function
@@ -42,17 +43,24 @@ def replaceText(row):
 def cleanStrike(row):
     return row['Strike'][:-1]
 
+#####
+# Cleaning up data 
+#####
 
-# Clean up data
-# drop: Action, Emojis
+# drop: Action, Emojis columns
 alertsDF = rawDF.drop(columns=['Actions','Emojis'], axis=1) 
+
+# clean up Time column i.e. alert time, leaving only the date
+alertsDF['Time'] = alertsDF['Time'].str[:-7]
 alertsDF['Time'] = pd.to_datetime(alertsDF['Time'])
+
+# remove trailing spaces 
 alertsDF['Option'].str.strip()
 
 alertsDF['Option'] = alertsDF['Option'].astype('str')
 alertsDF['Option'] = alertsDF['Option'].str.strip()
 
-#splt the Option col into symbol and strike
+#split the Option col into symbol and strike
 alertsDF[['Option', 'Strike']] = alertsDF['Option'].str.split('$', expand=True)
 alertsDF['Strike'] = alertsDF['Strike'].str.strip()
 alertsDF.rename(columns={'Option': 'Symbol'}, inplace=True)
@@ -87,7 +95,22 @@ alertsDF['Max Loss %'] = alertsDF['Max Loss %'].astype('float')
 
 alertsDF.fillna(0, inplace=True)
 
-# < 0
+###                                   ###
+##### END OF DATA CLEAN UP SECTION  #####
+###                                   ###
+
+#####
+# Adding compuited columns:
+# 'DTE' Date to Expiry of the option when the alert was first fired 
+alertsDF['DTE'] = (alertsDF['Expiry'] - alertsDF['Time']).dt.days
+
+
+
+#####
+# Create slices of alertsDF data based on the % return
+# for further analysis 
+#####
+# <= 0
 below0 = alertsDF.loc[alertsDF['Max Gain %'] <= 0]
 
 # 0 - 50
@@ -105,39 +128,34 @@ over200 = alertsDF.loc[(alertsDF['Max Gain %'] <= 1000) & (alertsDF['Max Gain %'
 # > 1000
 over1000 = alertsDF.loc[(alertsDF['Max Gain %'] > 1000)]
 
-
-
 #####
-# print statistical info
-#####
-#print(alertsDF.head(10))
-#print(alertsDF.describe())
-
-#print('average max gain', alertsDF['Max Gain %'].mean())
-
-
-#####
-# Plots and Graphs
+# Cleaninig up the slices to remove unnneeded columns i.e. strings 
+# to prepare for KMeans analysis 
 #####
 
-#alertsDF.plot.scatter(y = 'Max Loss %', x = 'IV')
-#alertsDF.plot.scatter(y = 'Max Loss %', x = 'Underlying')
-#alertsDF.plot.scatter(y = 'Max Loss %', x = 'Max Gain %')
+# all alerts preserved
+X_allAlerts = alertsDF.drop( ['Symbol', 'Expiry', 'Max Loss', 'Option Type', 'Sector', 'Underlying', '% Diff', 'OG ask', 'Time', 'Tier', 'Max Gain' ], axis=1 )
 
-#alertsDF.plot.hist('Max Gain %', bins = 20)
-#alertsDF['Max Gain %'].plot.hist(bins=80)
-#alertsDF['Max Loss %'].plot.hist(bins=80)
+# alerts based on optionType column i.e. Call or Put
+X_optionType = alertsDF.drop( ['Symbol', 'Expiry', 'Max Loss', 'Sector', 'Underlying', 'OG ask', 'Time', 'Tier', 'Max Gain' ], axis=1 )
+
+# alerts based on sector column
+X_Sector = alertsDF.drop( ['Symbol', 'Expiry', 'Max Loss', 'Option Type', 'Underlying', '% Diff', 'OG ask', 'Time', 'Tier', 'Max Gain' ], axis=1 )
+
+# ALL string columns removed 
+X_floatsOnly = alertsDF.drop( ['Symbol', 'Expiry', 'Max Loss', 'Option Type', 'Sector', 'Underlying', '% Diff', 'OG ask', 'Time', 'Tier', 'Max Gain' ], axis=1 )
 
 #####
-# Data slices for analysis
+# Plot returns over time 
 #####
-X_allAlerts = alertsDF.drop( ['Symbol', 'Expiry', 'Max Loss', 'Max Loss %', 'Option Type', 'Sector', 'Underlying', '% Diff', 'OG ask', 'Time', 'Tier', 'Max Gain' ], axis=1 )
+def plotReturns(bins): 
+    # max gain on calls & puts on 1 chart 
+    
+    #sns.lineplot(x = alertsDF['Time'], y = alertsDF['Max Gain %'], hue=alertsDF['Option Type'], )
+    
+    alertsDF['Max Gain %'].plot.hist(bins = bins, alpha=1)
 
-X_optionType = below0.drop( ['Symbol', 'Expiry', 'Max Loss', 'Max Loss %', 'Sector', 'Underlying', '% Diff', 'OG ask', 'Time', 'Tier', 'Max Gain' ], axis=1 )
-
-X_Sector = over200.drop( ['Symbol', 'Expiry', 'Max Loss', 'Max Loss %', 'Option Type', 'Underlying', '% Diff', 'OG ask', 'Time', 'Tier', 'Max Gain' ], axis=1 )
-
-X_floatsOnly = below0.drop( ['Symbol', 'Expiry', 'Max Loss', 'Max Loss %', 'Option Type', 'Sector', 'Underlying', '% Diff', 'OG ask', 'Time', 'Tier', 'Max Gain' ], axis=1 )
+    plt.show()
 
 #####
 # clustering - elbow method 
@@ -162,25 +180,64 @@ def elbowMethod(dataframeOfFloats):
     plt.show()
 
 #####
-# Clustering
+# Clustering & plotting
 #####
 def plotCluster(myDF, numClusters):
     km3 = KMeans(n_clusters = numClusters).fit(myDF)
     myDF['Labels'] = km3.labels_ 
     
     plt.figure(figsize=(12, 8))
-    sns.pairplot( myDF, hue = 'Labels')
-
+    #sns.pairplot( myDF, hue = 'Labels')
+    #sns.scatterplot(myDF['DTE'], myDF['Max Gain %'], hue=myDF['Labels'],
+    sns.scatterplot(myDF['Volume'], myDF['Max Gain %'], hue=myDF['Labels'],
+    palette=sns.color_palette('hls', numClusters))
+    plt.title('KMeans with 2 clusters')
+    print(myDF.head())
     plt.show()
 
-#elbowMethod(X_floatsOnly)
-plotCluster(X_floatsOnly, 3)
-
-#print(X_allAlerts.head())
 
 #####
-# plotting 
+# PRINTING stats
+#####
+print('Min Gain:', alertsDF['Max Gain %'].min())
+print('Max Gain:', alertsDF['Max Gain %'].max())
+print('Median:', alertsDF['Max Gain %'].median())
+
+#print(alertsDF.cov())
+#print(alertsDF.describe())
+
+print('Average max gain: ', alertsDF['Max Gain %'].mean())
+print('Average max loss: ', alertsDF['Max Loss %'].mean())
+
+#####
+# Plotting 
 #####
 # Plots scatter of each column 
 #sns.pairplot(X_optionType, hue='Option Type', aspect=1.5)
+
+#alertsDF.plot.scatter(y = 'Max Loss %', x = 'IV')
+#alertsDF.plot.scatter(y = 'Max Loss %', x = 'Underlying')
+alertsDF.plot.line(x = 'Time', y = 'Max Gain %')
+
+#alertsDF.plot.hist('Max Gain %', bins = 20)
+#alertsDF['Max Gain %'].plot.hist(bins=80)
+#alertsDF['Max Loss %'].plot.hist(bins=80)
+alertsDF_reduced = alertsDF[['Time', 'Option Type', 'Strike', 'Underlying', 'DTE', 'Expiry', 'Tier', 'OI', 'Volume', 'Max Loss %', 'Max Gain %']]
+ITMalerts = alertsDF_reduced.loc[((alertsDF_reduced['Strike'] > alertsDF_reduced['Underlying']) & (alertsDF_reduced['Option Type'] == 'Put')) | ( (alertsDF_reduced['Strike'] < alertsDF_reduced['Underlying']) & (alertsDF_reduced['Option Type'] == 'Call') )]
+
+print ( alertsDF_reduced.sort_values(by=['Time'], ascending=True) )
+print('')
+print('ITM Alerts')
+print ( ITMalerts.sort_values(by=['Time'], ascending=True) )
+
+#####
+# Plotting clusters
+#####
+#elbowMethod(X_floatsOnly)
+#plotCluster(X_floatsOnly, 2)
+
+#plt.show()
+#plotReturns(10)
+
+
 
